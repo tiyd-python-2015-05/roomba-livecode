22dbb7089a1b47acf680b4c730cdd8c407860f44
Clinton Dreisbach <clinton@dreisbach.us>
2015-05-20 11:32:19 -0400

    Added Roomba

--- simulation.py
+++ simulation.py
@@ -22,3 +22,24 @@ class Room:
 
     def in_bounds(self, x, y):
         return 0 <= x < self.width and 0 <= y < self.length
+
+class Roomba:
+    def __init__(self, x=0, y=0, angle=0):
+        self.x = x
+        self.y = y
+        self.angle = angle
+        self.speed = 1
+
+    @property
+    def position(self):
+        return (self.x, self.y)
+
+    @property
+    def next_position(self):
+        x_move = sin(radians(self.angle)) * self.speed
+        y_move = cos(radians(self.angle)) * self.speed
+        return (self.x + x_move, self.y + y_move)
+
+    def collide(self):
+        self.angle += 45
+        self.angle %= 360
